bdef31f2d3c35ee4acf8798c5a5ee497aa35cfc8
Ingo Marquardt <ingo.marquardt@gmx.de>
2017-03-20 20:34:48 +0100

    Modifications to draining model.

--- ds_drainModel.py
+++ ds_drainModel.py
@@ -56,15 +56,15 @@ at all depth levels.
 
     Layer IV:
     vecTrsf4 = np.array([0.0, 0.0, 2.2, 1.3, 1.3])
-    
+
     Layer II/III:
     vecTrsf23 = np.array([0.0, 0.0, 0.0, 1.7, 0.7])
-    
+
     Layer I:
     vecTrsf23 = np.array([0.0, 0.0, 0.0, 0.0, 1.6])
 
 Multiplying these vectors with the local neuronal activity in each layer, and
-adding up the result gives the predicted haemodynamic signal.
+adding up the result gives the predicted haemodynamic signal (forward model).
 
 Reference:
 Markuerkiaga, I., Barth, M., & Norris, D. G. (2016). A cortical vascular model
@@ -90,58 +90,133 @@ Markuerkiaga, I., Barth, M., & Norris, D. G. (2016). A cortical vascular model
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
 
-#varEmp6 = 1.9
-#varEmp5 = 2.1
-#varEmp4 = 3.1
-#varEmp23 = 3.8
-#varEmp1 = 4.4
+import numpy as np
+from scipy.interpolate import griddata
+
+
+# *** Define parameters
+
+# Number of equi-volume depth levels in the input data:
+varNumDpth = 11
+
+# Path of depth-profile to correct:
+strPthPrf = '/home/john/PhD/ParCon_Depth_Data/Higher_Level_Analysis/v1.npy'
+
+
+# *** Draining model (Markuerkiaga et al. 2016)
+
+# Forward model (neuronal activity --> fMRI signal):
+
+# The following vectors contain the predicted fMRI signal at each depth level
+# if the neuronal activity at each depth level is equal (one).
+
+## Layer VI:
+#vecTrsfVI = np.array([1.9, 0.6, 0.6, 0.5, 0.5])
+## Layer V:
+#vecTrsfV = np.array([0.0, 1.5, 0.3, 0.3, 0.3])
+## Layer IV:
+#vecTrsfIV = np.array([0.0, 0.0, 2.2, 1.3, 1.3])
+## Layer II/III:
+#vecTrsfII_III = np.array([0.0, 0.0, 0.0, 1.7, 0.7])
+## Layer I:
+#vecTrsfI = np.array([0.0, 0.0, 0.0, 0.0, 1.6])
+## Stacking the transfer functions:
+#aryTrsf = np.vstack((vecTrsfVI, vecTrsfV, vecTrsfIV, vecTrsfII_III, vecTrsfI))
 
 # Layer VI:
-vecTrsf6 = np.array([1.9, 0.6, 0.6, 0.5, 0.5])
+vecTrsfVI = np.array([0.0, 0.6, 0.6, 0.5, 0.5])
+varTrsfVI = 1.9
+vecTrsfVI = np.divide(vecTrsfVI, varTrsfVI)
 
 # Layer V:
-vecTrsf5 = np.array([0.0, 1.5, 0.3, 0.3, 0.3])
+vecTrsfV = np.array([0.0, 0.0, 0.3, 0.3, 0.3])
+varTrsfV = 1.5
+vecTrsfV = np.divide(vecTrsfV, varTrsfV)
 
 # Layer IV:
-vecTrsf4 = np.array([0.0, 0.0, 2.2, 1.3, 1.3])
+vecTrsfIV = np.array([0.0, 0.0, 0.0, 1.3, 1.3])
+varTrsfIV = 2.2
+vecTrsfIV = np.divide(vecTrsfIV, varTrsfIV)
 
 # Layer II/III:
-vecTrsf23 = np.array([0.0, 0.0, 0.0, 1.7, 0.7])
+vecTrsfII_III = np.array([0.0, 0.0, 0.0, 0.0, 0.7])
+varTrsfII_III = 1.7
+vecTrsfII_III = np.divide(vecTrsfII_III, varTrsfII_III)
 
 # Layer I:
-vecTrsf1 = np.array([0.0, 0.0, 0.0, 0.0, 1.6])
+vecTrsfI = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+varTrsfI = 1.6
+# vecTrsfI = np.divide(vecTrsfI, varTrsfI)
+
+# Stacking the transfer functions:
+aryTrsf = np.vstack((vecTrsfVI, vecTrsfV, vecTrsfIV, vecTrsfII_III, vecTrsfI))
+lstTrsf = [varTrsfVI, varTrsfV, varTrsfIV, varTrsfII_III, varTrsfI]
 
 
-vecHdm = vecTrsf6 + vecTrsf5 + vecTrsf4 + vecTrsf23 + vecTrsf1
+# *** Load depth profile from disk
+
+# Array with single-subject depth sampling results, of the form
+# arySubDpthMns[idxSub, idxCondition, idxDpth].
+aryDpthSnSb = np.load(strPthPrf)
+
+# Across-subjects mean:
+aryDpth = np.mean(aryDpthSnSb, axis=0)
+
+# Number of conditions:
+varNumCon = aryDpth.shape[0]
 
 
-#Layer VI:
-varCrct6 = varEmp6 / 1.9
+# *** Interpolation (downsampling)
 
-#Layer V:
-varCrct5 = (varEmp5
-            - (0.6 * varCrct6)) / 1.5
+# The empirical depth profiles are defined at more depth levels than the
+# draining model. We downsample the empirical depth profiles to the number of
+# depth levels of the model.
 
-# Layer IV:
-varCrct4 = (varEmp4
-            - (0.3 * varCrct5)
-            - (0.6 * varCrct6)) / 2.2
+# Relative thickness of the layers (layer VI, 20%; layer V, 10%; layer IV,
+# 40%; layer II/III, 20%; layer I, 10%; Markuerkiaga et al. 2016).
+# lstThck = [0.2, 0.1, 0.4, 0.2, 0.1]
+# From the relative thickness, we derive the relative position of the layers
+# (we set the position of each layer to the sum of all lower layers plus half
+# its own thickness):
+vecPosMdl = np.array([0.1, 0.25, 0.5, 0.8, 0.95])
 
-# Layer II/III:
-varCrct23 = (varEmp23
-             - (1.3 * varCrct4)
-             - (0.3 * varCrct5)
-             - (0.5 * varCrct6)) / 1.7
+# Position of empirical datapoints:
+vecPosEmp = np.linspace(0.0, 1.0, num=varNumDpth, endpoint=True)
 
-# Layer I:
-varCrct1 = (varEmp1
-            - (0.7 * varCrct23)
-            - (1.3 * varCrct4)
-            - (0.3 * varCrct5)
-            - (0.5 * varCrct6)) / 1.6
+aryDpth5 = np.zeros((varNumCon, 5))
 
-vecEmp = np.array([varEmp6, varEmp5, varEmp4, varEmp23, varEmp1])
-vecCrct = np.array([varCrct6, varCrct5, varCrct4, varCrct23, varCrct1])
-aryComb = np.array([vecEmp, vecCrct]).T
+for idxCon in range(0, varNumCon):
+
+    # The actual itnerpolation:
+    aryDpth5[idxCon] = griddata(vecPosEmp,
+                                aryDpth[idxCon, :],
+                                vecPosMdl,
+                                method='linear')
+
+# Array for corrected depth profiles:
+aryCrct = np.copy(aryDpth5)
+
+for idxCon in range(0, varNumCon):
+
+    aryDrain = np.zeros(aryTrsf.shape)
+    
+    for idxDpth in range(0, 5):
+        
+        aryDrain = np.multiply(aryTrsf[idxDpth, :],
+                               aryDpth5[idxCon, idxDpth])
+        
+        print(aryDrain)
+
+        aryCrct[idxCon, :] = np.subtract(aryCrct[idxCon, :],
+                                         aryDrain)
+
+    for idxDpth in range(0, 5):
+
+        aryCrct[idxCon, idxDpth] = np.divide(aryCrct[idxCon, idxDpth],
+                                             lstTrsf[idxDpth])
+
+aa1 = aryDpth5.T
+aa2 = aryCrct.T
+
+print('lala')

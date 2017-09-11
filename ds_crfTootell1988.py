c5b5b3d9e5831a5f35dc0688713502ff7d31e84c
Ingo Marquardt <ingo.marquardt@gmx.de>
2017-09-11 12:54:35 +0200

    Option to smooth depth profiles from Tootell et al. (1988) in order to facilitate comparision with fMRI depth profiles.

--- ds_crfTootell1988.py
+++ ds_crfTootell1988.py
@@ -49,8 +49,28 @@ Tootell, R. B., Hamilton, S. L., & Switkes, E. (1988). Functional anatomy of
 import numpy as np
 from ds_crfFit import crf_fit
 from ds_pltAcrDpth import funcPltAcrDpth
+from scipy.interpolate import interp1d
+from scipy.ndimage.filters import gaussian_filter1d
 
 print('-CRF fitting on data from Tootell et al. (1988)')
+
+
+# ----------------------------------------------------------------------------
+# *** Parameters
+
+# Width of Gaussian (FWHM) for smoothing in order to simulate partial volume
+# effect on depth profiles (for comparison with fMRI profiles). If zero, no
+# smoothing is performed. Unit is the ratio of cortical depth, i.e. if 0.1,
+# FWHM is 10% of cortical depth.
+varSd = 0.1
+
+# Number of x-values for which to solve the function when calculating model
+# fit:
+varNumX = 1000
+
+
+# ----------------------------------------------------------------------------
+# *** Preparations
 
 # Stimulus contrast levels (independent variable):
 vecEmpX = np.array([0.08, 0.18, 0.4, 1.0])
@@ -77,12 +97,12 @@ lstLayPos = [670.0,  # Layer 3
              55.0]  # Layer 6
 
 # Get number of layers:
-#varNumLayers = len(dictEmpY.keys())
-varNumLayers = len(lstLayPos)
-
-# Number of x-values for which to solve the function when calculating model
-# fit:
-varNumX=1000
+if np.greater(float(varSd), 0.0):
+    # If smoothing is applied, the depth-profiles are upsampled before
+    # smoothing. The plots are then created at the upsampled resolution.
+    varNumLayers = int(max(lstLayPos))
+else:
+    varNumLayers = len(lstLayPos)
 
 # Array for modelled y values, of the form aryMdlY[idxLayer, idxContrast]:
 aryMdlY = np.zeros((varNumLayers, varNumX))
@@ -99,24 +119,94 @@ aryRes = np.zeros((varNumLayers, varNumConEmp))
 # Vector for relative laminar depth of results (for plot):
 vecLayPos = np.zeros((varNumLayers))
 
+
+# ----------------------------------------------------------------------------
+# *** Smoothing of depth profiles
+
+# Smooth profiles in order to simulate fMRI partial volume effect:
+if np.greater(float(varSd), 0.0):
+
+    # Cortical depth is set to 1000, based on the reference frame of the
+    # empirical values from the literature:
+    varNumIntp = 1000.0
+
+    # Position of original datapoints (before interpolation):
+    vecPosOrig = np.asarray(lstLayPos)
+
+    # Positions at which to sample (interpolate) depth profiles:
+    vecPosIntp = np.linspace(0.0, varNumIntp, num=1000, endpoint=True)
+
+    # Empirical data (from literature) from list to array:
+    aryEmpY = np.asarray(lstEmpY)
+
+    # Create function for interpolation:
+    func_interp = interp1d(vecPosOrig,
+                           aryEmpY,
+                           kind='linear',
+                           axis=0,
+                           fill_value='extrapolate')
+
+# interp1d - Possibly use other parameters?
+# kind : str or int, optional
+# Specifies the kind of interpolation as a string (‘linear’, ‘nearest’, ‘zero’,
+# ‘slinear’, ‘quadratic’, ‘cubic’ where ‘zero’, ‘slinear’, ‘quadratic’ and
+# ‘cubic’ refer to a spline interpolation of zeroth, first, second or third
+# order) or as an integer specifying the order of the spline interpolator to
+# use. Default is ‘linear’.
+
+    # Apply interpolation function:
+    aryEmpIntY = func_interp(vecPosIntp)
+
+    # Scale the standard deviation of the Gaussian kernel:
+    varSdSc = np.float64(varNumIntp) * varSd
+
+    # Smooth interpolated depth profiles:
+    aryEmpSmthY = gaussian_filter1d(aryEmpIntY,
+                                    varSdSc,
+                                    axis=0,
+                                    order=0,
+                                    mode='nearest')
+
+    # After interpolation, spacing of laminar positions is linear, with the
+    # maximum value equal to the position of the outermost original datapoint:
+    vecLayPos = np.linspace(0.0,
+                            float(max(lstLayPos)),
+                            num=int(max(lstLayPos)))
+
+
+# ----------------------------------------------------------------------------
+# *** Contrast response function fitting
+
 # Loop through layers & fit function:
 for idxLay in range(varNumLayers):
 
     print(('---Layer ' + str(idxLay + 1) + ' of ' + str(varNumLayers)))
 
-    # Reshape to fit expected function input:
-    vecEmpY = lstEmpY[idxLay].reshape((1, varNumConEmp))
+    # The data structure is different if smoothing was applied:
+    if np.greater(float(varSd), 0.0):
+        vecEmpY = aryEmpSmthY[idxLay, :].reshape((1, varNumConEmp))
+    else:
+        # Reshape to fit expected function input:
+        vecEmpY = lstEmpY[idxLay].reshape((1, varNumConEmp))
 
     # Rescale values:
     vecEmpY = np.multiply(vecEmpY, 0.001)
 
     aryMdlY[idxLay, :], vecHlfMax[idxLay], vecSemi[idxLay], \
-        aryRes[idxLay, :] = crf_fit(vecEmpX, vecEmpY,
-        strFunc='power', varNumX=varNumX)
+        aryRes[idxLay, :] = crf_fit(vecEmpX, vecEmpY, strFunc='power',
+                                    varNumX=varNumX)
 
-    # Put relative laminar position of current layer into vector (for
-    # plotting):
-    vecLayPos[idxLay] = lstLayPos[idxLay]
+    # Only fill vector with relative laminar positions if no smoothing is
+    # performed (otherwise the spacing is linear):
+    if not(np.greater(float(varSd), 0.0)):
+
+        # Put relative laminar position of current layer into vector (for
+        # plotting):
+        vecLayPos[idxLay] = lstLayPos[idxLay]
+
+
+# ----------------------------------------------------------------------------
+# *** Prepare plots
 
 # Rescale laminar position (to fit with plot settings):
 vecLayPos = np.subtract(vecLayPos, 55.0)
@@ -130,8 +220,12 @@ vecHlfMax = vecHlfMax.reshape((1, varNumLayers))
 # Dummy array for error bars:
 aryError = np.zeros(vecSemi.shape)
 
+
+# ----------------------------------------------------------------------------
+# *** Plot results
+
 # Path for figure:
-strPath = '/home/john/PhD/Tex/Tootell_1988/semiTootell1988.svg'
+strPath = '/Users/john/Desktop/Tootell_Smooth/semiTootell1988_smth_0p1.png'
 
 # Plot semisaturation contrast:
 funcPltAcrDpth(vecSemi, aryError, varNumLayers, 1, 80.0, 0.0, 0.5, True,
@@ -140,10 +234,11 @@ funcPltAcrDpth(vecSemi, aryError, varNumLayers, 1, 80.0, 0.0, 0.5, True,
                varNumLblY=6, varXmin=0.0, varXmax=1.0)
 
 # Path for figure:
-strPath = '/home/john/PhD/Tex/Tootell_1988/hlfmaxTootell1988.svg'
+strPath = '/Users/john/Desktop/Tootell_Smooth/hlfmaxTootell1988_smth_0p1.png'
 
-# Plot semisaturation contrast:
+# Plot response at 50% contrast:
 funcPltAcrDpth(vecHlfMax, aryError, varNumLayers, 1, 80.0, 0.0, 1.0, False,
                [''], 'Cortical depth', 'Response [a.u.]',
                'Response at 50% contrast', False, strPath, vecX=vecLayPos,
                varNumLblY=6, varXmin=0.0, varXmax=1.0)
+# ----------------------------------------------------------------------------

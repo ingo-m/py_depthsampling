4e8e4ceaba43d90e04aea84d158c9f60cbc102ea
Ingo Marquardt <ingo.marquardt@gmx.de>
2017-09-29 20:22:26 +0200

    Adjusted depth-sampling for PacMan, including both hemispheres and MT.

--- ds_main_PacMan.py
+++ ds_main_PacMan.py
@@ -57,7 +57,6 @@ import numpy as np
 import multiprocessing as mp
 from ds_acrSubsGetData import funcAcrSubGetSubsData
 from ds_pltAcrSubsMean import funcPltAcrSubsMean
-from ds_linRegAcrSubs import funcLinRegAcrSubs
 # *****************************************************************************
 
 print('-Visualisation of depth sampling results')
@@ -66,7 +65,10 @@ print('-Visualisation of depth sampling results')
 # *** Define parameters
 
 # Region of interest ('v1' or 'v2'):
-strRoi = 'v1'
+strRoi = 'mt'
+
+# Hemisphere ('lh' or 'rh'):
+strHmsph = 'lh'
 
 # List of subject identifiers:
 lstSubIds = ['20161221']
@@ -77,14 +79,14 @@ lstCon = ['Static', 'Dynamic', 'DmS']
 lstConLbl = ['Static', 'Dynamic', 'DmS']
 
 # Base path of first set of vtk files with depth-sampled data, e.g. parameter
-# estimates (with subject ID and stimulus level left open):
-strVtkDpth01 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/lh/pe_{}.vtk'  # noqa
+# estimates (with subject ID, hemisphere, and stimulus level left open):
+strVtkDpth01 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/pe_{}.vtk'  # noqa
 
 # (1)
 # Base path of csv files with ROI definition (i.e. patch of cortex selected on
 # the surface, e.g. V1 or V2) - i.e. the first vertex selection criterion (with
-# subject ID & ROI left open):
-strCsvRoi = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/lh/{}.csv'  #noqa
+# subject ID, hemisphere, and ROI left open):
+strCsvRoi = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/{}.csv'  #noqa
 
 # (2)
 # Use second selection criterion defined at one depth level (legacy function):
@@ -99,19 +101,23 @@ varThrSlct02 = 100.0
 # (3)
 # Use third selection criterion (vertices that are BELOW threshold at any depth
 # level are excluded):
-lgcSlct03 = True
+lgcSlct03 = False
 # Path of vtk files with 3rd vertex selection criterion. This vtk file is
-# supposed to contain one set of data values for each depth level.
-strVtkSlct03 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/lh/R2_multi.vtk'  #noqa
+# supposed to contain one set of data values for each depth level. (With
+# subject ID and hemisphere left open.)
+if strHmsph == 'lh':
+    strVtkSlct03 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/pRF_ovrlp_centre_right_visual_field_smoothdata.vtk'  #noqa
+elif strHmsph == 'rh':
+    strVtkSlct03 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/pRF_ovrlp_centre_left_visual_field_smoothdata.vtk'  #noqa
 # Threshold for vertex selection:
-varThrSlct03 = 0.08
+varThrSlct03 = 0.9
 
 # (4)
 # Use fourth selection criterion (vertices that are BELOW threshold at any
 # depth level are excluded):
 lgcSlct04 = True
-# Path of exclusion mask:
-strVtkSlct04 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/lh/combined_mean.vtk'  #noqa
+# Path of exclusion mask (with subject ID and hemisphere left open):
+strVtkSlct04 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/combined_mean.vtk'  #noqa
 # Threshold for vertex selection:
 varThrSlct04 = 7000.0
 
@@ -120,11 +126,11 @@ varThrSlct04 = 7000.0
 # distribution?
 lgcVtk02 = True
 # How many vertices to select for each subject?
-lstNumVrtx = [500] * len(lstSubIds)
-
+lstNumVrtx = [1000] * len(lstSubIds)
 # Base name of second set of vtk files with depth-sampled data, e.g. z-values
-# (with subject ID left open):
-strVtkDpth02 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/lh/zstats_Dynamic.vtk'  #noqa
+# (with subject ID and hemisphere left open):
+# strVtkDpth02 = '/home/john/PhD/PacMan_Depth_Data/{}/cbs_distcor/{}_extrasession_retinotopy/R2_multi_smoothdata.vtk'  #noqa
+strVtkDpth02 = '/home/john/PhD/PacMan_Depth_Data/20161221/cbs_distcor/lh_extrasession_retinotopy/zstats_Dynamic.vtk'  #noqa
 
 # (6)
 # Use PE range?
@@ -155,7 +161,11 @@ varNumLne = 2
 strTitle = strRoi.upper()
 
 # Limits of y-axis for single subject plots (list of tuples, [(Ymin, Ymax)]):
-lstLimY = [(-3.0, 3.0)] * len(lstSubIds)
+# lstLimY = [(-2.0, 5.0)] * len(lstSubIds)
+# lstLimY = [(-1.0, 3.0)] * len(lstSubIds)
+# lstLimY = [(-200.0, 100.0)] * len(lstSubIds)
+# lstLimY = [(-300.0, 100.0)] * len(lstSubIds)
+lstLimY = [(0.0, 500.0)] * len(lstSubIds)
 
 # Limits of y-axis for across subject plot:
 varAcrSubsYmin = -3.0
@@ -163,13 +173,13 @@ varAcrSubsYmax = 3.0
 
 # Label for axes:
 strXlabel = 'Cortical depth level (equivolume)'
-strYlabel = 'z-value'
+strYlabel = 'fMRI signal [a.u.]'
 
-# Output path for plots - prfix:
+# Output path for plots - prefix:
 strPltOtPre = '/home/john/PhD/PacMan_Plots/plots_{}/'.format(strRoi)
 
 # Output path for plots - suffix:
-strPltOtSuf = '_R2multi_0p08_combinedmean_7000_z_1000.png'
+strPltOtSuf = '_{}_{}.png'.format(strHmsph, strRoi)
 
 # Figure scaling factor:
 varDpi = 80.0
@@ -182,28 +192,10 @@ varDpi = 80.0
 varNormIdx = 0
 
 # Normalise by division?
-lgcNormDiv = True
-
-# Linear regression?
-lgcLinReg = False
-
-# Constrast vector for simple linear regression model (array with one value per
-# condition, e.g. per stimulus contrast level):
-vecLinRegMdl = np.array([-3.0, -1.0, 1.0, 3.0])
-
-# P-threshold (uncorrected) for regression model (will be corrected for
-# multiple comparison automatically): *** NOT IMPLEMENTED
-varLinRegP = 0.001
-
-# Range of y-axis for regression plots:
-varLinRegYmin = 0.0
-varLinRegYmax = 0.18
-
-# Linear regression plot - label for axes:
-strLinRegYlabel = 'Regression coefficient'
+lgcNormDiv = False
 
 # Output path for depth samling results (within subject means):
-strDpthMeans = '/home/john/PhD/PacMan_Depth_Data/Higher_Level_Analysis/{}.npy'.format(strRoi)  #noqa
+strDpthMeans = '/home/john/PhD/PacMan_Depth_Data/Higher_Level_Analysis/{}_{}.npy'.format(strRoi, strHmsph)  #noqa
 
 # Maximum number of processes to run in parallel: *** NOT IMPLEMENTED
 # varPar = 10
@@ -238,17 +230,19 @@ for idxSub in range(0, varNumSubs):
 
     # Create list with complete file names for the data to be depth-sampled:
     lstVtkDpth01 = [strVtkDpth01.format(lstSubIds[idxSub],
+                                        strHmsph,
                                         strTmp) for strTmp in lstCon]
 
     # Complete file paths:
-    strCsvRoiTmp = strCsvRoi.format(lstSubIds[idxSub], strRoi)
+    strCsvRoiTmp = strCsvRoi.format(lstSubIds[idxSub], strHmsph, strRoi)
     strVtkSlct02Tmp = strVtkSlct02.format(lstSubIds[idxSub])
-    strVtkSlct03Tmp = strVtkSlct03.format(lstSubIds[idxSub])
-    strVtkSlct04Tmp = strVtkSlct04.format(lstSubIds[idxSub])
+    strVtkSlct03Tmp = strVtkSlct03.format(lstSubIds[idxSub], strHmsph)
+    strVtkSlct04Tmp = strVtkSlct04.format(lstSubIds[idxSub], strHmsph)
 
     # Create list with complete file names for multi-level data distribution
     # criterion II:
-    lstVtkDpth02 = [strVtkDpth02.format(lstSubIds[idxSub])] * varNumCon
+    lstVtkDpth02 = [strVtkDpth02.format(lstSubIds[idxSub],
+                                        strHmsph)] * varNumCon
 
     # Prepare processes that plot & return single subject data:
     lstPrcs[idxSub] = \
@@ -356,27 +350,4 @@ funcPltAcrSubsMean(arySubDpthMns,
                    strTitle,
                    strPltOtPre,
                    strPltOtSuf)
-# *****************************************************************************
-
-
-# *****************************************************************************
-# *** Plot mean simple regression (across-subjects)
-
-if lgcLinReg:
-
-    print('---Plot results - simple linear regression (across-subjects).')
-
-    funcLinRegAcrSubs(arySubDpthMns,
-                      vecLinRegMdl,
-                      varNumSubs,
-                      varNumDpth,
-                      strTitle,
-                      strXlabel,
-                      strLinRegYlabel,
-                      varLinRegYmin,
-                      varLinRegYmax,
-                      varLinRegP,
-                      varDpi,
-                      strPltOtPre,
-                      strPltOtSuf)
 # *****************************************************************************

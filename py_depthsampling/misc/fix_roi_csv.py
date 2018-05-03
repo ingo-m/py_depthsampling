7ac8e96d546df98a1779a49b5f9a6e17ffd3dcfc
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-05-03 22:16:25 +0200

    PEP8 and bugfix re first and last vertices in list.

--- py_depthsampling/misc/fix_roi_csv.py
+++ py_depthsampling/misc/fix_roi_csv.py
@@ -26,16 +26,16 @@ import csv
 import numpy as np
 
 
-def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='POINTS',
-                varNumLne=1, varRnd=1):
+def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1,  #noqa
+                strPrcdCoor='POINTS', varNumLne=1, varRnd=1):
     """
     Fix indices in ROI definitions.
 
     Parameters
     ----------
     strCsvRoi : string
-        Base path of csv files with ROI definition (i.e. patch of cortex selected on
-        the surface, e.g. V1 or V2).
+        Base path of csv files with ROI definition (i.e. patch of cortex
+        selected on the surface, e.g. V1 or V2).
     strCsvRoiOut : string
         Output path of modified csv file.
     strVtkIn : string
@@ -46,13 +46,15 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
     varNumHdrRoi : int
         Number of header lines in ROI CSV file.
     strPrcdCoor : string
-        Beginning of string which precedes vertex coordinates in data vtk files.
+        Beginning of string which precedes vertex coordinates in data vtk
+        files.
     varNumLne : int
-        Number of lines between string ('strPrcdCoor') and coordinate in vtk file.
+        Number of lines between string ('strPrcdCoor') and coordinate in vtk
+        file.
     varRnd : int
         Number of decimal places after rounding for comparison of vertex
         coordinates between csv ROI and vtk mesh.
-    
+
     Returns
     -------
     This function has no return value. The indices in the csv file are updated,
@@ -60,21 +62,26 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
 
     Notes
     -----
-    Regions of interest (ROIs) created with paraview on a cortical surface may have
-    inconsistent vertex indices with respect to vtk meshes. This may be the case
-    if existing ROIs are loaded from disk (loading a paraview 'state' file) and
-    then edited. Here, such edited ROIs are loaded,
+    Regions of interest (ROIs) created with paraview on a cortical surface may
+    have inconsistent vertex indices with respect to vtk meshes. This may be
+    the case if existing ROIs are loaded from disk (loading a paraview 'state'
+    file) and then edited. Here, such edited ROIs are loaded, and their
+    indicies are replaced with those from a reference VTK mesh file, based on
+    the correspondence of vertex coordinates between the VTK mesh and the CSV
+    ROI.
     """
     print('-Fixing vertex indices in ROI CSV file.')
     print(('---CSV ROI: ' + strCsvRoi))
+
     # -------------------------------------------------------------------------
     # *** Load vtk file
-    
+
     # Load single-depth-level vtk file (e.g. 'polar_angle_thr.vtk' that was
     # used to delineate ROI). Different to the standard ROI loading module
     # (i.e. 'load_vtk_single'), we do not only load the data, but the indices
     # and coordinates of vertices.
-        # Open vtk file:
+
+    # Open vtk file:
     fleVtkIn = open(strVtkIn, 'r')
 
     # Read file  vtk file:
@@ -170,11 +177,11 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
         # corresponding to parameter value (e.g. polar angle), vertex ID,
         # x-coordinate, y-coordinate, z-coordinate.
         lstTmp = lstCsvRoi[idxCsv].split(',')
-        
+
         # Coordinates:
         varTmpX = np.around(float(lstTmp[2]), decimals=varRnd)
         varTmpY = np.around(float(lstTmp[3]), decimals=varRnd)
-        varTmpZ = np.around(float(lstTmp[4]), decimals=varRnd)        
+        varTmpZ = np.around(float(lstTmp[4]), decimals=varRnd)
 
         # Create sets of vertex coordinates in vtk mesh that coincide with the
         # coordinate of the current vertex from the csv file:
@@ -196,49 +203,51 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
             # Coordinates:
             varTmpX = np.around(float(lstTmp[2]), decimals=(varRnd + 1))
             varTmpY = np.around(float(lstTmp[3]), decimals=(varRnd + 1))
-            varTmpZ = np.around(float(lstTmp[4]), decimals=(varRnd + 1))        
-    
-            # Create sets of vertex coordinates in vtk mesh that coincide with the
-            # coordinate of the current vertex from the csv file:
+            varTmpZ = np.around(float(lstTmp[4]), decimals=(varRnd + 1))
+
+            # Create sets of vertex coordinates in vtk mesh that coincide with
+            # the coordinate of the current vertex from the csv file:
             setTmpX = set(np.where(np.equal(vecXrnd02, varTmpX))[0])
             setTmpY = set(np.where(np.equal(vecYrnd02, varTmpY))[0])
             setTmpZ = set(np.where(np.equal(vecZrnd02, varTmpZ))[0])
-    
-            # List of intersections (i.e. vertices that have the same x, y, and z
-            # coordinate as the current vertex from the csv file):
+
+            # List of intersections (i.e. vertices that have the same x, y, and
+            # z coordinate as the current vertex from the csv file):
             lstInt = list(set.intersection(setTmpX, setTmpY, setTmpZ))
 
             if (len(lstInt) > 1):
 
                 # Error message:
-                strErrMsg = ('---Error: More than one vertex from reference VTK '
-                             + 'file has the same coordinates as vertex number '
+                strErrMsg = ('---Error: More than one vertex from reference '
+                             + 'VTK file has the same coordinates as vertex '
+                             + 'number '
                              + str(idxCsv)
                              + ' from CSV ROI file. There should only be one '
-                             + 'vertex per coordinate. Consider reduced rounding '
-                             + 'of vertex coordinates.')
+                             + 'vertex per coordinate. Consider reduced '
+                             + 'rounding of vertex coordinates.')
                 raise ValueError(strErrMsg)
 
         if (len(lstInt) == 1):
-    
-            # The list of vertices at intersection of coordinates has length one,
-            # containing the index of the vertex (in the vtk mesh) corresponding to
-            # the current vertex in the CSV ROI. We replace the (possibly wrong)
-            # vertex index in the CSV ROI with the vtk mesh index.
-    
+
+            # The list of vertices at intersection of coordinates has length
+            # one, containing the index of the vertex (in the vtk mesh)
+            # corresponding to the current vertex in the CSV ROI. We replace
+            # the (possibly wrong) vertex index in the CSV ROI with the vtk
+            # mesh index.
+
             # Convert index to string:
             strTmpVtkIdx = str(lstInt[0])
-    
+
             # Replace old (wrong) index with new index:
             lstTmp[1] = strTmpVtkIdx
-    
-            # Converte list of strings to string (same as original line from csv
-            # file, just with new index):
+
+            # Converte list of strings to string (same as original line from
+            # csv file, just with new index):
             strTmp = ','.join(lstTmp)
-    
+
             # Put modified line into csv list:
             lstCsvRoi[idxCsv] = strTmp
-            
+
         elif (len(lstInt) == 0):
 
             # Remember vertices without correspondence:
@@ -250,42 +259,50 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
     # If no corresponding points could be found, first check whether new
     # (modified) vertex IDs of preceeding and suceeding vertices are in
     # sequence (e.g. if preceeding ID is 60075 and suceeding ID is 60077,
-    # assign ID = 60076 to respective vertex). If this is not the case,
-    # remove this vertex from the ROI. Loop through missing elements in reverse order,
+    # assign ID = 60076 to respective vertex). If this is not the case, remove
+    # this vertex from the ROI. Loop through missing elements in reverse order,
     # because list indices change when removing elements.
     for idxErr in list(reversed(lstErr)):
-        # Preceeding index:
-        strTmpPre = lstCsvRoi[(idxErr - 1)].split(',')[1]
-        # Suceesing index:
-        strTmpSce = lstCsvRoi[(idxErr + 1)].split(',')[1]
 
-        # Is succeeding index = preceeding index plus two?
-        if ((int(strTmpPre) + 2) == int(strTmpSce)):
+        # Skip first and last vertices (there would be no preceeding or
+        # suceeding value for interpolation):
+        if (idxErr > 0) and (lstCsvRoi > idxErr):
 
-            print('+++' + strTmpPre + ' ' + lstCsvRoi[idxErr].split(',')[1] + ' ' + strTmpSce)
-            
-            # Get current line of csv file (split into list of strings,
-            # corresponding to parameter value (e.g. polar angle), vertex ID,
-            # x-coordinate, y-coordinate, z-coordinate.
-            lstTmp = lstCsvRoi[idxErr].split(',')
-            
-            # Convert index to string:
-            strTmpVtkIdx = str((int(strTmpPre) + 1))
-    
-            # Replace old (wrong) index with new index:
-            lstTmp[1] = strTmpVtkIdx
-    
-            # Converte list of strings to string (same as original line from csv
-            # file, just with new index):
-            strTmp = ','.join(lstTmp)
-    
-            # Put modified line into csv list:
-            lstCsvRoi[idxErr] = strTmp
-            
+            # Preceeding index:
+            strTmpPre = lstCsvRoi[(idxErr - 1)].split(',')[1]
+            # Suceesing index:
+            strTmpSce = lstCsvRoi[(idxErr + 1)].split(',')[1]
+
+            # Is succeeding index = preceeding index plus two?
+            if ((int(strTmpPre) + 2) == int(strTmpSce)):
+
+                # Get current line of csv file (split into list of strings,
+                # corresponding to parameter value (e.g. polar angle), vertex
+                # ID, x-coordinate, y-coordinate, z-coordinate.
+                lstTmp = lstCsvRoi[idxErr].split(',')
+
+                # Convert index to string:
+                strTmpVtkIdx = str((int(strTmpPre) + 1))
+
+                # Replace old (wrong) index with new index:
+                lstTmp[1] = strTmpVtkIdx
+
+                # Converte list of strings to string (same as original line
+                # from csv file, just with new index):
+                strTmp = ','.join(lstTmp)
+
+                # Put modified line into csv list:
+                lstCsvRoi[idxErr] = strTmp
+
+            else:
+
+                # Delete vertex.
+                del(lstCsvRoi[idxErr])
+                # Increment error counter:
+                varErrCnt += 1
+
         else:
 
-            print('---' + strTmpPre + ' ' + lstCsvRoi[idxErr].split(',')[1] + ' ' + strTmpSce)
-            
             # Delete vertex.
             del(lstCsvRoi[idxErr])
             # Increment error counter:
@@ -303,7 +320,7 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
     objCsvOt = open(strCsvRoiOut, 'w')
 
     # Save manipulated list to disk:
-    csvOt = csv.writer(objCsvOt,delimiter=' ', lineterminator='\n')
+    csvOt = csv.writer(objCsvOt, delimiter=' ', lineterminator='\n')
 
     # Write output list data to file (row by row):
     for strTmp in lstCsvRoi:
@@ -314,13 +331,37 @@ def fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn, varNumHdrRoi=1, strPrcdCoor='
     # -------------------------------------------------------------------------
 
 
+# -----------------------------------------------------------------------------
+# *** Run function
 
-strCsvRoi = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/20171023/cbs/rh/v2.csv'
-strCsvRoiOut = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/20171023/cbs/rh/v2_mod.csv'
-varNumHdrRoi = 1
-strVtkIn = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/20171023/cbs/rh/polar_angle_thr.vtk'
-strPrcdCoor ='POINTS'
-varNumLne = 1
-varRnd = 1
+# Region of interest ('v1' or 'v2'):
+lstRoi = ['v1', 'v2', 'v3']
 
-fix_roi_csv(strCsvRoi, strCsvRoiOut, strVtkIn)
+# Hemispheres ('lh' or 'rh'):
+lstHmsph = ['lh', 'rh']
+
+# List of subject identifiers:
+lstSubIds = ['20171023',  # '20171109',
+             '20171204_01',
+             '20171204_02',
+             '20171211',
+             '20171213',
+             '20180111',
+             '20180118']
+
+# Path of input csv files (subject ID, hemisphere, and ROI left open):
+strCsvRoi = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/{}.csv'
+
+# Path of output csv files (subject ID, hemisphere, and ROI left open):
+strCsvRoiOut = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/{}_mod.csv'
+
+# Path of reference vtk file (subject ID and hemisphere left open):
+strVtkIn = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/polar_angle_thr.vtk'  #noqa
+
+for idxSub in lstSubIds:
+    for idxHmpsh in lstHmsph:
+        for idxRoi in lstRoi:
+
+            fix_roi_csv(strCsvRoi.format(idxSub, idxHmpsh, idxRoi),
+                        strCsvRoiOut.format(idxSub, idxHmpsh, idxRoi),
+                        strVtkIn.format(idxSub, idxHmpsh))

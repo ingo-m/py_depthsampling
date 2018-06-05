Pixel-wise normalisation of visual space. Correct labelling of x- and y-axis in visual space plot (achieve by transposing visual space array).

py_depthsampling/project/main.py
@@ -46,7 +46,7 @@ lstSubIds = ['20171023',  # '20171109',
 lstMdl = ['']  # , '_deconv_model_1']
 
 # ROI ('v1' or 'v2'):
-lstRoi = ['v1', 'v2']
+lstRoi = ['v1']  # , 'v2']
 
 # Output path & prefix for plots (ROI and condition left open):
 strPthPltOt = '/home/john/Dropbox/PacMan_Plots/project/{}_{}'  #noqa
@@ -66,6 +66,7 @@ strTtl = 'Visual field projection'
 
 # Condition levels (used to complete file names):
 lstCon = ['Pd_sst', 'Cd_sst', 'Ps_sst']
+# lstCon = ['polar_angle', 'x_pos', 'y_pos', 'SD', 'R2']
 
 # Condition labels:
 # lstConLbl = ['PacMan Dynamic Sustained',
@@ -75,6 +76,7 @@ lstCon = ['Pd_sst', 'Cd_sst', 'Ps_sst']
 # Path of vtk mesh with data to project into visual space (e.g. parameter
 # estimates; subject ID, hemisphere, and contion level left open).
 strPthData = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/feat_level_2_{}_cope.vtk'  #noqa
+# strPthData = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/pRF_results_{}.vtk'  #noqa
 
 # Path of vtk mesh with R2 values from pRF mapping (at multiple depth levels;
 # subject ID and hemisphere left open).
@@ -306,8 +308,10 @@ for idxMdl in range(len(lstMdl)):  #noqa
                 for idxPrc in range(varPar):
                     lstPrcs[idxPrc].join()
 
-                # List for results after re-ordering:
+                # List for results after re-ordering (visual space arrays and
+                # normalisation arrays):
                 lstVslSpc = [None] * varPar
+                lstNorm = [None] * varPar
 
                 # Put output into correct order (unnecessary in this context
                 # but kept for consistency):
@@ -318,14 +322,24 @@ for idxMdl in range(len(lstMdl)):  #noqa
 
                     # Put fitting results into list, in correct order:
                     lstVslSpc[varTmpIdx] = lstRes[idxRes][1]
+                    lstNorm[varTmpIdx] = lstRes[idxRes][2]
 
                 # Visual space array (2D array with bins of locations in visual
                 # space):
                 aryVslSpc = np.zeros((varNumX, varNumY))
 
+                # Array for normalisation (parameter estimates are summed up
+                # over the visual field; the normalisation array is needed to
+                # normalise the sum):
+                aryNorm = np.zeros((varNumX, varNumY))
+
                 # Add up results from separate processes:
                 for idxPrc in range(varPar):
                     aryVslSpc = np.add(lstVslSpc[idxPrc], aryVslSpc)
+                    aryNorm = np.add(lstNorm[idxPrc], aryNorm)
+
+                # Normalise:
+                aryVslSpc = np.divide(aryVslSpc, aryNorm)
 
                 # Save results to disk:
                 np.save(strPthNpyTmp, aryVslSpc)

py_depthsampling/project/plot.py
@@ -39,10 +39,13 @@ def plot(aryVslSpc, strTtl, strXlabel, strYlabel, strPathOut):
     # Round:
     # varMin = (np.floor(varMin * 10.0) / 10.0)
     # varMax = (np.ceil(varMax * 10.0) / 10.0)
-    varMin = (np.floor(varMin * 0.1) / 0.1)
-    varMax = (np.ceil(varMax * 0.1) / 0.1)
-    # varMin = np.floor(varMin)
-    # varMax = np.ceil(varMax)
+    # varMin = (np.floor(varMin * 0.1) / 0.1)
+    # varMax = (np.ceil(varMax * 0.1) / 0.1)
+    varMin = np.floor(varMin)
+    varMax = np.ceil(varMax)
+
+    if np.less_equal(0.0, varMin):
+        varMin = -1.0
 
     # Create main figure:
     fig01 = plt.figure(figsize=(4.0, 3.0),
@@ -127,7 +130,7 @@ def plot(aryVslSpc, strTtl, strXlabel, strYlabel, strPathOut):
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     # Plot correlation coefficients of current depth level:
-    pltTmpCorr = plt.imshow(aryVslSpc,
+    pltTmpCorr = plt.imshow(aryVslSpc.T,
                             interpolation='none',  # 'bicubic',
                             origin='lower',
                             norm=objClrNorm,

py_depthsampling/project/project_par.py
@@ -37,6 +37,10 @@ def project_par(idxPrc, vecData, vecX, vecY, vecSd, vecR2, varThrR2, varNumX,
     # space):
     aryVslSpc = np.zeros((varNumX, varNumY))
 
+    # Array for normalisation (parameter estimates are summed up over the
+    # visual field; the array is needed to normalise the sum):
+    aryNorm = np.zeros((varNumX, varNumY))
+
     # Vector with visual space coordinates of elements in `aryVslSpc`:
     vecCorX = np.linspace(varExtXmin, varExtXmax, num=varNumX,
                           endpoint=True)
@@ -80,6 +84,9 @@ def project_par(idxPrc, vecData, vecX, vecY, vecSd, vecR2, varThrR2, varNumX,
             # Scale Gaussian to have its maximum at one:
             # aryTmpGauss = np.divide(aryTmpGauss, np.max(aryTmpGauss))
 
+            # Add non-scaled Gaussian to normalisation array:
+            aryNorm = np.add(aryNorm, aryTmpGauss)
+
             # Multiply current data value (e.g. parameter estimate)
             # with Gaussian:
             aryTmpGauss = np.multiply(aryTmpGauss, vecData[idxVrtx])
@@ -88,7 +95,7 @@ def project_par(idxPrc, vecData, vecX, vecY, vecSd, vecR2, varThrR2, varNumX,
             aryVslSpc = np.add(aryVslSpc, aryTmpGauss)
 
     # Create list containing subject data, and the process ID:
-    lstOut = [idxPrc, aryVslSpc]
+    lstOut = [idxPrc, aryVslSpc, aryNorm]
 
     # Put output to queue:
     queOut.put(lstOut)
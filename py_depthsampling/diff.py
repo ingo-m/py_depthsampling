9eafd8f776222403768dccfa501e5bf1910803ca
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-06-21 20:21:05 +0200

    Plot mean of between stimulus conditions difference, with SEM.

---
+++ py_depthsampling/diff.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Plot difference between conditions.
+
+Plot mean of between stimulus conditions difference, with SEM.
+"""
+
+# Part of py_depthsampling library
+# Copyright (C) 2018  Ingo Marquardt
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from py_depthsampling.diff.diff_sem import diff_sem
+
+
+# -----------------------------------------------------------------------------
+# *** Define parameters
+
+# Which parameter to plot - 'mean' or 'median'.
+strParam = 'mean'
+
+# Which draining model to plot ('' for none):
+lstMdl = ['', '_deconv_model_1']
+
+# Meta-condition (within or outside of retinotopic stimulus area):
+lstMetaCon = ['stimulus']
+# lstMetaCon = ['periphery']
+
+# ROI ('v1', 'v2', or 'v3'):
+lstRoi = ['v1', 'v2', 'v3']
+
+# Hemisphere ('rh' or 'lh'):
+lstHmsph = ['rh']
+
+# Path of corrected depth-profiles (meta-condition, ROI, hemisphere,
+# condition, and model index left open):
+strPthData = '/home/john/Dropbox/PacMan_Depth_Data/Higher_Level_Analysis/{}/{}_{}_{}{}.npz'  #noqa
+
+# Output path & prefix for plots (meta-condition, ROI, ROI, hemisphere, and
+# model index left open):
+strPthPltOt = '/home/john/Dropbox/PacMan_Plots/diff/{}/{}/{}_{}{}'  #noqa
+
+# Output path for single subject plot, (ROI, metacondition, hemisphere, drain
+# model, and condition left open):
+# strPthPtlSnglOt = '/home/john/Dropbox/PacMan_Plots/diff_sngle/{}_{}_{}{}_{}'
+
+# File type suffix for plot:
+strFlTp = '.png'
+
+# Figure scaling factor:
+varDpi = 100.0
+
+# Label for axes:
+strXlabel = 'Cortical depth level (equivolume)'
+strYlabel = 'Signal change [%]'
+
+# Condition levels (used to complete file names):
+lstCon = ['Pd_sst', 'Ps_sst', 'Cd_sst']
+# lstCon = ['Pd_sst', 'Ps_sst', 'Cd_sst', 'Ps_sst_plus_Cd_sst']
+# lstCon = ['Pd_sst', 'Ps_sst', 'Cd_sst']
+# lstCon = ['Pd_trn', 'Ps_trn', 'Cd_trn']
+
+# Condition labels:
+lstConLbl = lstCon
+
+# Which conditions to compare (list of tuples with condition indices):
+lstDiff = [(0, 1), (0, 2), (2, 1)]
+# lstDiff = [(0, 1), (0, 2), (2, 1), (0, 3)]
+
+# Padding around labelled values on y:
+tplPadY = (0.05, 0.25)
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# *** Loop through ROIs / conditions
+
+# Loop through models, ROIs, hemispheres, and conditions to create plots:
+for idxMtaCn in range(len(lstMetaCon)):  #noqa
+    for idxMdl in range(len(lstMdl)):  #noqa
+        for idxRoi in range(len(lstRoi)):
+            for idxHmsph in range(len(lstHmsph)):
+
+                # Limits of axes need to be adjusted based on ROI,
+                # condition, hemisphere.
+
+                if idxRoi == 0:  # v1
+
+                    if lstMetaCon[idxMtaCn] == 'stimulus':
+                        if lstMdl[idxMdl] == '':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                        if lstMdl[idxMdl] == '_deconv_model_1':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                    if lstMetaCon[idxMtaCn] == 'periphery':
+                        if lstMdl[idxMdl] == '':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                        if lstMdl[idxMdl] == '_deconv_model_1':
+                            varYmin = -0.5
+                            varYmax = 0.5
+
+                elif (idxRoi == 1) or (idxRoi == 3):  # v2 & v3
+
+                    if lstMetaCon[idxMtaCn] == 'stimulus':
+                        if lstMdl[idxMdl] == '':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                        if lstMdl[idxMdl] == '_deconv_model_1':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                    if lstMetaCon[idxMtaCn] == 'periphery':
+                        if lstMdl[idxMdl] == '':
+                            varYmin = -0.5
+                            varYmax = 0.5
+                        if lstMdl[idxMdl] == '_deconv_model_1':
+                            varYmin = -0.5
+                            varYmax = 0.5
+
+                # Create average plots:
+                diff_sem(strPthData.format(lstMetaCon[idxMtaCn],
+                                           lstRoi[idxRoi],
+                                           lstHmsph[idxHmsph],
+                                           '{}',
+                                           lstMdl[idxMdl]),
+                         (strPthPltOt.format(lstMetaCon[idxMtaCn],
+                                             lstRoi[idxRoi],
+                                             lstRoi[idxRoi],
+                                             lstHmsph[idxHmsph],
+                                             lstMdl[idxMdl])
+                          + strFlTp),
+                         lstCon,
+                         lstConLbl,
+                         varYmin=varYmin,
+                         varYmax=varYmax,
+                         tplPadY=tplPadY,
+                         strXlabel=strXlabel,
+                         strYlabel=strYlabel,
+                         lgcLgnd=True,
+                         lstDiff=lstDiff,
+                         strParam=strParam)
+
+            # Create single subject plot(s):
+            # boot_plot_sngl(strPthData.format(lstMetaCon[idxMtaCn],
+            #                                  lstRoi[idxRoi],
+            #                                  lstHmsph[idxHmsph],
+            #                                  '{}',
+            #                                  lstMdl[idxMdl]),
+            #                (strPthPtlSnglOt.format(lstRoi[idxRoi],
+            #                                        lstMetaCon[idxMtaCn],
+            #                                        lstHmsph[idxHmsph],
+            #                                        lstMdl[idxMdl],
+            #                                        '{}')
+            #                 + strFlTp),
+            #                lstCon,
+            #                lstConLbl,
+            #                strXlabel='Cortical depth level (equivolume)',
+            #                strYlabel='Subject',
+            #                lstDiff=lstDiff)
+# -----------------------------------------------------------------------------
---
+++ py_depthsampling/diff/diff_sem.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+"""Function of the depth sampling pipeline."""
+
+# Part of py_depthsampling library
+# Copyright (C) 2018  Ingo Marquardt
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import numpy as np
+import rpy2.robjects as robjects
+from py_depthsampling.plot.plt_dpth_prfl import plt_dpth_prfl
+
+
+def diff_sem(objDpth, strPath, lstCon, lstConLbl, strTtl='', varYmin=0.0,  #noqa
+             varYmax=2.0, tplPadY=(0.0, 0.0),
+             strXlabel='Cortical depth level (equivolume)',
+             strYlabel='fMRI signal change [arbitrary units]',
+             lgcLgnd=False, lstDiff=None, vecNumInc=None, strParam='mean'):
+    """
+    Plot across-subject cortical depth profiles with SEM.
+
+    Parameters
+    ----------
+    objDpth : np.array or str
+        Array with single-subject cortical depth profiles, of the form:
+        aryDpth[idxSub, idxCondition, idxDpth]. Either a numpy array or a
+        string with the path to an npy file containing the array.
+    strPath : str
+        Output path for plot.
+    lstCon : list
+        Abbreviated condition levels used to complete file names (e.g. 'Pd').
+    lstConLbl : list
+        List containing condition labels (strings). Number of condition labels
+        has to be the same as number of conditions in `lstCon`.
+    strTtl : str
+        Plot title.
+    varYmin : float
+        Minimum of Y axis.
+    varYmax : float
+        Maximum of Y axis.
+    tplPadY : tuple
+        Padding around labelled values on y.
+    strXlabel : str
+        Label for x axis.
+    strYlabel : str
+        Label for y axis.
+    lgcLgnd : bool
+        Whether to show a legend.
+    lstDiff : list or None
+        If None, the depth profiles are plotted separately for each condition.
+        If a list of tuples of condition indices is provided, the difference
+        between the two conditions is calculated, and is plotted. The the
+        second condition from the tuple is subtracted from the first (e.g. if
+        lstDiff = [(0, 1)], then condition 1 is subtracted from condition 0).
+    vecNumInc : np.array
+        1D array with weights for weighted averaging over subjects (e.g. number
+        of vertices per subject). If the array is loaded from disk (i.e. if
+        `objDpth` is the path to an `*.npz` file stored on disk), `vecNumInc`
+        has to be in the `*.npz` file. If `objDpth` is a numpy array containing
+        the data, `vecNumInc` should also be provided as an input arguments.
+        Otherwise, weights are set to be equal across subjects.
+    strParam : string
+        Which parameter to plot; 'mean' or 'median'. If `strParam = 'median'`,
+        an R function is imported for calculating the weighted median.
+        Dependency (in python): `rpy2`, dependency (in R): `spatstat`.
+
+    Returns
+    -------
+    None : None
+        This function has no return value.
+
+    Notes
+    -----
+    Plot across-subject mean or median cortical depth profiles with SEM.
+
+    Function of the depth sampling pipeline.
+    """
+    # -------------------------------------------------------------------------
+    # *** Prepare bootstrapping
+
+    # Test whether the input is a numpy array or a string (with the path to a
+    # numpy array):
+    lgcAry = (type(objDpth) == np.ndarray)
+    lgcStr = (type(objDpth) == str)
+
+    # If input is a string, load array from npy file:
+    if lgcAry:
+        aryDpth = objDpth
+        # If weights are not provided, set equal weight of one for each
+        # subject:
+        if vecNumInc is None:
+            vecNumInc = np.ones((aryDpth.shape[0]))
+
+    elif lgcStr:
+        # Load array for first condition to get dimensions:
+        objNpz = np.load(objDpth.format(lstCon[0]))
+        aryTmpDpth = objNpz['arySubDpthMns']
+        # Number of subjects:
+        varNumSub = aryTmpDpth.shape[0]
+        # Get number of depth levels from input array:
+        varNumDpth = aryTmpDpth.shape[1]
+        # Number of conditions:
+        varNumCon = len(lstCon)
+        # Array for depth profiles of form aryDpth[subject, condition, depth]:
+        aryDpth = np.zeros((varNumSub, varNumCon, varNumDpth))
+        # Load single-condition arrays from disk:
+        for idxCon in range(varNumCon):
+            objNpz = np.load(objDpth.format(lstCon[idxCon]))
+            aryDpth[:, idxCon, :] = objNpz['arySubDpthMns']
+        # Array with number of vertices (for weighted averaging across
+        # subjects), shape: vecNumInc[subjects].
+        vecNumInc = objNpz['vecNumInc']
+
+    else:
+        print(('---Error in bootPlot: input needs to be numpy array or path '
+               + 'to numpy array.'))
+
+    # Get number of subjects from input array:
+    varNumSub = aryDpth.shape[0]
+    # Get number of conditions from input array:
+    varNumCon = aryDpth.shape[1]
+    # Get number of depth levels from input array:
+    varNumDpth = aryDpth.shape[2]
+
+    # Define R function for calculation of weighted median:
+    strFuncR = """
+     funcR <- function(lstData, lstWght){
+     library(spatstat)
+     varWm <- weighted.median(lstData, lstWght)
+     return(varWm)
+     }
+    """
+    objFuncR = robjects.r(strFuncR)
+
+    if not(lstDiff is None):
+        # Set number of comparisons:
+        varNumCon = len(lstDiff)
+
+    # Array for SEM:
+    arySem = np.zeros((varNumCon, varNumDpth))
+
+    # ------------------------------------------------------------------------
+    # *** Calculate parameters
+
+    if lstDiff is None:
+
+        if strParam == 'mean':
+
+            # Sum of weights over subjects (i.e. total number of vertices
+            # across subjects; for scaling).
+            varSum = np.sum(vecNumInc)
+
+            # Multiply depth profiles by weights (weights are broadcasted over
+            # conditions and depth levels):
+            aryTmp = np.multiply(aryDpth, vecNumInc[:, None, None])
+
+            # Sum over subjects, and scale by total number of vertices:
+            aryEmpMne = np.divide(
+                                  np.sum(aryTmp, axis=0),
+                                  varSum
+                                  )
+
+        elif strParam == 'median':
+
+            # Array for weighted median difference between conditions:
+            aryEmpMne = np.zeros((varNumCon, varNumDpth))
+
+            # Calculate weighted median in R (yes this is slow):
+            for idxCon in range(varNumCon):
+                for idxDpth in range(varNumDpth):
+                    aryEmpMne[idxCon, idxDpth] = \
+                        objFuncR(list(aryDpth[:, idxCon, idxDpth]),
+                                 list(vecNumInc))[0]
+
+    else:
+
+        # Empirical mean difference between conditions:
+        aryEmpMne = np.zeros((varNumCon, varNumDpth))
+
+        for idxDiff in range(varNumCon):
+
+            if strParam == 'mean':
+
+                # Sum of weights over subjects (i.e. total number of vertices
+                # across subjects; for scaling).
+                varSum = np.sum(vecNumInc)
+
+                # Difference in cortical depth profiles between conditions:
+                aryDiff = np.subtract(aryDpth[:, lstDiff[idxDiff][0], :],
+                                      aryDpth[:, lstDiff[idxDiff][1], :])
+
+                # Multiply depth profiles by weights (weights are broadcasted
+                # over depth levels):
+                aryDiff = np.multiply(aryDiff, vecNumInc[:, None])
+
+                # Sum over subjects, and scale by total number of vertices:
+                aryEmpMne[idxDiff, :] = np.divide(
+                                                  np.sum(aryDiff, axis=0),
+                                                  varSum
+                                                  )
+
+                # Formular for SEM according to Franz & Loftus (2012). Standard
+                # errors and confidence intervals in within-subjects designs:
+                # generalizing Loftus and Masson (1994) and avoiding the biases
+                # of alternative accounts. Psychonomic Bulletin & Review,
+                # 19(3), p. 398.
+                arySem[idxDiff, :] = \
+                    np.sqrt(
+                            np.multiply(
+                                        np.divide(
+                                                  1.0,
+                                                  np.multiply(
+                                                              float(varNumSub),
+                                                              (float(varNumSub) - 1.0)
+                                                              )
+                                                  ),
+                                        np.sum(
+                                               np.power(
+                                                        np.subtract(
+                                                                    np.subtract(
+                                                                                aryDpth[:, lstDiff[idxDiff][0], :],
+                                                                                aryDpth[:, lstDiff[idxDiff][1], :]
+                                                                                ),
+                                                                    np.mean(
+                                                                            np.subtract(
+                                                                                        aryDpth[:, lstDiff[idxDiff][0], :],
+                                                                                        aryDpth[:, lstDiff[idxDiff][1], :]),
+                                                                            axis=0
+                                                                            ),
+                                                                    ),
+                                                        2.0
+                                                        ),
+                                               axis=0
+                                               )
+                                        )
+                             )
+
+            elif strParam == 'median':
+
+                # Calculate weighted median difference between conditions in R
+                # (yes this is slow):
+                for idxDiff in range(varNumCon):
+
+                    # Difference in cortical depth profiles between conditions:
+                    aryDiff = np.subtract(aryDpth[:, lstDiff[idxDiff][0], :],
+                                          aryDpth[:, lstDiff[idxDiff][1], :])
+
+                    for idxDpth in range(varNumDpth):
+                        aryEmpMne[idxDiff, idxDpth] = \
+                            objFuncR(list(aryDiff[:, idxDpth]),
+                                     list(vecNumInc))[0]
+                            
+                # TODO: SEM
+
+        # Create condition labels for differences:
+        lstDiffLbl = [None] * varNumCon
+        for idxDiff in range(varNumCon):
+            lstDiffLbl[idxDiff] = ((lstConLbl[lstDiff[idxDiff][0]])
+                                   + ' minus '
+                                   + (lstConLbl[lstDiff[idxDiff][1]]))
+        lstConLbl = lstDiffLbl
+
+    # ------------------------------------------------------------------------
+    # *** Plot results
+
+    plt_dpth_prfl(aryEmpMne, arySem, varNumDpth, varNumCon, 80.0, varYmin,
+                  varYmax, False, lstConLbl, strXlabel, strYlabel, strTtl,
+                  lgcLgnd, strPath, varSizeX=1800.0, varSizeY=1600.0,
+                  varNumLblY=5, varPadY=tplPadY)
+    # ------------------------------------------------------------------------

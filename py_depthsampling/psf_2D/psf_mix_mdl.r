a0fa165ac404216c7beb90f46b103e28f5bea2a2
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-07-04 20:40:10 +0200

    Mixed regression on parameters of cortical depth point spread function. Work in progress.

--- py_depthsampling/psf_2D/psf_mix_mdl.r
+++ py_depthsampling/psf_2D/psf_mix_mdl.r
@@ -1,10 +1,42 @@
+# Mixed regression on parameters of cortical depth point spread function.
+#
+# Data is loaded from csv file containing dataframe. Dataframe csv file can be
+# created using `py_depthsampling.psf_2D.psf_2D_main.py`.
 
-library(ggplot2)
+# Part of py_depthsampling library
+# Copyright (C) 2018  Ingo Marquardt
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
-strCsv <- '/Users/john/Dropbox/PacMan_Depth_Data/Higher_Level_Analysis/psf/dataframe.csv'
+# Requires:
+# install.packages('lme4')
+# library(lme4)
 
-dfSlope = read.csv(strCsv)
+# Path of input csv file:
+strCsv <- '/home/john/Dropbox/PacMan_Depth_Data/Higher_Level_Analysis/psf_2D/dataframe.csv'
 
-colnames(dfSlope)
+# Read data from disk:
+dfPsf = read.csv(strCsv)
 
-ggplot(data=dfSlope, mapping=aes(x=ROI, y=Slope, group=Depth,)) + geom_boxplot()
+colnames(dfPsf)
+
+# Create mixed effects model:
+# objModel <- lmer(Width ~ ROI + Condition + (1|Depth), data=dfPsf)
+
+objModel <- lm(Width ~ ROI + Condition + Depth, data=dfPsf)
+
+# objModel <- aov(Width ~ ROI + Condition + Depth, data=dfPsf)
+
+summary(objModel)
+

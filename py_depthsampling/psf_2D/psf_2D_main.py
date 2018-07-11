Bugfix calculation of residuals (residuals have to be calculated with respect to target, not reference).

py_depthsampling/psf_2D/psf_2D_main.py
@@ -226,7 +226,7 @@ for idxRoi in range(varNumRoi):
                 # ** Plot residuals visual field projection
 
                 # Calculate residuals:
-                aryRes = np.subtract(aryDeep, aryFit)
+                aryRes = np.subtract(aryTrgt, aryFit)
 
                 # Output path for plot:
                 strPthPltOtTmp = (strPthPltVfp.format((lstRoi[idxRoi]
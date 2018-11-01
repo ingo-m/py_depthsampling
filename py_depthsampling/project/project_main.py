913bc3fdae3df7f17384a440cd41e94ff6ccb3c4
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-11-01 17:44:20 +0100

    Flow control - base decision whether to scale to PSC on condition string.

--- py_depthsampling/project/project_main.py
+++ py_depthsampling/project/project_main.py
@@ -163,7 +163,7 @@ def project(strRoi, strCon, strDpth, strDpthLbl, strPthNpy, varNumSub,
         # of filtered_func_data)."
 
         # Only perform scaling if the data is from an FSL cope file:
-        if (('cope' in strPthData) or ('_pe' in strPthData)):
+        if (('cope' in strCon) or ('_pe' in strCon)):
             print('--Convert cope to percent signal change.')
 
             # The peak-peak height depends on the predictor (i.e.

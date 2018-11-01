2a840d81377177f69ebfd26c37aa89b38fe49c38
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-11-01 17:43:35 +0100

    Adjust I/O for surface project.

--- py_depthsampling/project/project_ert.py
+++ py_depthsampling/project/project_ert.py
@@ -20,34 +20,28 @@ from py_depthsampling.project.project_main import project
 
 # Load/save existing projection from/to (ROI, condition, depth level label, and
 # volume index left open):
-strPthNpy = '/home/john/PhD/PacMan_Depth_Data_Dropbox_Relieve/project_ert/{}_{}_{}_volume_{}.npy'  #noqa
+strPthNpy = '/home/john/Dropbox/Surface_Depth_Data/Higher_Level_Analysis/project_ert/{}_{}_{}_volume_{}.npy'  #noqa
 
 # Output path & prefix for plots (ROI, condition, depth level label left open):
-strPthPltOt = '/home/john/Dropbox/PacMan_Plots/project/ert/{}_{}_{}'  #noqa
+strPthPltOt = '/home/john/PhD/Surface_Plots/project_ert/{}_{}_{}'  #noqa
 
 # Region of interest ('v1' or 'v2'):
 lstRoi = ['v1', 'v2', 'v3']
 
 # List of subject identifiers:
-lstSubIds = ['20171023',  # '20171109',
-             '20171204_01',
-             '20171204_02',
-             '20171211',
-             '20171213',
-             '20180111',
-             '20180118']
+lstSubIds = ['20181029']
 
 # Condition levels (used to complete file names):
-lstCon = ['pacman_dynamic', 'pacman_static', 'control_dynamic']
+lstCon = ['bright_square', 'dark_square', 'kanizsa']
 
 # Condition labels (for plot legend):
-lstConLbl = ['Pacman dynamic', 'Pacman static', 'Control dynamic']
+lstConLbl = ['Bright square', 'Dark square', 'Kanizsa']
 
 # Number of cortical depths:
 varNumDpth = 11
 
 # Number of timepoints:
-varNumVol = 19
+varNumVol = 20
 
 # Beginning of string which precedes vertex data in data vtk files (i.e. in the
 # statistical maps):
@@ -55,13 +49,6 @@ strPrcdData = 'SCALARS'
 
 # Number of lines between vertex-identification-string and first data point:
 varNumLne = 2
-
-# Limits of y-axis:
-varAcrSubsYmin = -0.04
-varAcrSubsYmax = 0.02
-
-# Number of labels on y-axis:
-varYnum = 4
 
 # Convert y-axis values to percent (i.e. divide label values by 100)?
 # lgcCnvPrct = False
@@ -84,41 +71,41 @@ varDpi = 80.0
 
 # Path of csv file with ROI definition (subject ID, hemisphere, and ROI left
 # open).
-strCsvRoi = '/home/john/PhD/GitLab/PacMan/analysis/{}/08_depthsampling/{}/{}_mod.csv'  #noqa
+strCsvRoi = '/home/john/PhD/GitLab/surface/analysis/{}/08_depthsampling/{}/{}.csv'  #noqa
 
 # Path of vtk mesh with data to project into visual space (e.g. parameter
 # estimates; subject ID, hemisphere, condition, and condition level left open).
-strPthData =  '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}_era/{}/aryErt_{}.npy'  #noqa
+strPthData =  '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}_era/{}/aryErt_{}.npy'  #noqa
 
 # Path of mean EPI (for scaling to percent signal change; subject ID and
 # hemisphere left open). Not used here, but necessary because of modularity
 # (multiprocessing function does not take kwargs).
-strPthMneEpi = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/combined_mean.vtk'  #noqa
+strPthMneEpi = '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}/combined_mean.vtk'  #noqa
 
 # Path of vtk mesh with R2 values from pRF mapping (at multiple depth levels;
 # subject ID and hemisphere left open).
-strPthR2 = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/pRF_results_R2.vtk'  #noqa
+strPthR2 = '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}/pRF_results_R2.vtk'  #noqa
 
 # Path of vtk mesh with pRF sizes (at multiple depth levels; subject ID and
 # hemisphere left open).
-strPthSd = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/pRF_results_SD.vtk'  #noqa
+strPthSd = '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}/pRF_results_SD.vtk'  #noqa
 
 # Path of vtk mesh with pRF x positions at multiple depth levels; subject ID
 # and hemisphere left open).
-strPthX = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/pRF_results_x_pos.vtk'  #noqa
+strPthX = '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}/pRF_results_x_pos.vtk'  #noqa
 
 # Path of vtk mesh with pRF y positions at multiple depth levels; subject ID
 # and hemisphere left open).
-strPthY = '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs/{}/pRF_results_y_pos.vtk'  #noqa
+strPthY = '/media/sf_D_DRIVE/MRI_Data_PhD/09_surface/{}/cbs/{}/pRF_results_y_pos.vtk'  #noqa
 
 # Extent of visual space from centre of the screen in negative x-direction
 # (i.e. from the fixation point to the left end of the screen) in degrees of
 # visual angle.
-varExtXmin = -5.19
+varExtXmin = -8.3
 # Extent of visual space from centre of the screen in positive x-direction
 # (i.e. from the fixation point to the right end of the screen) in degrees of
 # visual angle.
-varExtXmax = 5.19
+varExtXmax = 8.3
 # Extent of visual space from centre of the screen in negative y-direction
 # (i.e. from the fixation point to the lower end of the screen) in degrees of
 # visual angle.
@@ -135,7 +122,7 @@ varThrR2 = 0.15
 # Number of bins for visual space representation in x- and y-direction (ratio
 # of number of x and y bins should correspond to ratio of size of visual space
 # in x- and y-directions).
-varNumX = 200
+varNumX = 320
 varNumY = 200
 
 # Minimum and maximum of colour bar:

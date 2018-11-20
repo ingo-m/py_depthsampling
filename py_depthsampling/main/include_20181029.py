932f60fc0154d55099c5391e64dfe945d2b822d3
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-11-20 19:35:01 +0100

    Style / docstring changes.

--- py_depthsampling/main/include_20181029.py
+++ py_depthsampling/main/include_20181029.py
@@ -8,6 +8,9 @@ The pilot session did not include the 'Kanizsa rotated' control condition.
 Here, the data from the pilot session are integrated into the npz file
 containing data from subsequent sessions. The npz file for the pilot session
 first needs to be created separately from that of the following sessions.
+
+In order to include the pilot session, perform this adjustment before running
+the spatial deconvolution.
 """
 
 # Part of py_depthsampling library
@@ -37,7 +40,8 @@ import numpy as np
 # pilot session. (Meta-conditoin, ROI, condition left open.)
 strPthNpz01 = '/home/john/Dropbox/Surface_Depth_Data/Higher_Level_Analysis/{}/{}_{}.npz'  #noqa
 
-# Path of npz file with depth profiles of pilot session (Meta-conditoin, ROI, condition left open.)
+# Path of npz file with depth profiles of pilot session (Meta-conditoin, ROI,
+# condition left open.)
 strPthNpz02 = '/home/john/Dropbox/Surface_Depth_Data/Higher_Level_Analysis/{}/{}_{}_20181029.npz'  #noqa
 
 # Meta-condition (within or outside of retinotopic stimulus area):
@@ -46,10 +50,10 @@ lstMtaCon = ['centre', 'edge', 'diamond', 'background']
 # Region of interest ('v1' or 'v2'):
 lstRoi = ['v1', 'v2', 'v3']
 
+# Conditions:
 lstCon = ['bright_square_sst_pe',
           'kanizsa_rotated_sst_pe',
           'kanizsa_sst_pe']
-lstCon = ['kanizsa_rotated_sst_pe']
 
 # Array index of matching session:
 varSesId01 = 2
@@ -68,17 +72,17 @@ for idxMtaCon in lstMtaCon:
 
             # -----------------------------------------------------------------
             # ### Load data
-            
+
             # Input path - npz file all sessions (except pilot session):
             strTmp01 = strPthNpz01.format(idxMtaCon, idxRoi, idxCon)
 
             # Input path - npz file pilot session:
             strTmp02 = strPthNpz02.format(idxMtaCon, idxRoi, idxCon)
-        
+
             # Control condition 'Kanizsa rotated' was not included in the pilot
             # session. Fill the array with the corresponding data from the
             # same subject from a separate session (20181108).
-                
+
             # Load depth profiles from disk - all sessions (except pilot
             # session):
             objNpz01 = np.load(strTmp01)
@@ -114,16 +118,16 @@ for idxMtaCon in lstMtaCon:
                 # Number of depth levels:
                 varNumDpth = aryDpth01.shape[1]
 
-                # Concatenate depth profiles:                
+                # Concatenate depth profiles:
                 aryDpth = np.concatenate(
                     (aryDpth01, aryDpth01[varSesId01, :].reshape(1,
-                    varNumDpth)), axis=0)
+                     varNumDpth)), axis=0)
 
                 # Concatenate number of vertices:
                 vecNumInc = np.concatenate(
                     (vecNumInc01, vecNumInc01[varSesId01].reshape(1)), axis=0)
-            # -----------------------------------------------------------------               
-                
+            # -----------------------------------------------------------------
+
             # -----------------------------------------------------------------
             # ### Save to disk
 
@@ -131,7 +135,3 @@ for idxMtaCon in lstMtaCon:
                      arySubDpthMns=aryDpth,
                      vecNumInc=vecNumInc)
             # -----------------------------------------------------------------
-
-
-
-

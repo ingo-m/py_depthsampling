ecd1bbd4720026dc9e2b116b7a8fff6433234fca
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-01-21 20:35:00 +0100

    Linear mixed effects modelling.

---
+++ py_depthsampling/lme/r_depth_analysis.r
@@ -0,0 +1,60 @@
+
+library(nlme)
+
+# Read CSV into R
+objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/depth_data.csv', header=TRUE, sep=';')
+
+head(objDf)
+
+# lme(PSC~ROI+Condition+Depth, objDf, random=~Depth|Subject)
+
+# objLme = lme(PSC ~ ROI + Condition + Depth,
+#              objDf,
+#              random=(~1|Subject),
+#              method='ML',
+#              correlation=corCAR1(form=(~1|Depth)))
+
+# Linear mixed-effects model:
+mdl_lme = lme(PSC ~ ROI * Condition * Depth,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Depth)))
+
+# Linear model using generalised least squares:
+mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
+              objDf,
+              method='ML',
+              correlation=corCAR1(form=(~1|Depth)))
+
+summary(mdl_gls)
+
+nlme:::summary.gls(mdl_gls)$tTable
+
+
+
+# mdlNull = lme(PSC ~ ROI + Condition + Depth +
+#               ROI:Condition +
+#               ROI:Depth +
+#               Condition:Depth,
+#               objDf,
+#               random=(~1|Subject),
+#               method='ML')
+# 
+# mdlFull = lme(PSC ~ ROI + Condition + Depth +
+#               ROI:Condition +
+#               ROI:Depth +
+#               Condition:Depth +
+#               Condition:Depth:ROI,
+#               objDf,
+#               random=(~1|Subject),
+#               method='ML')
+# 
+# anova(mdlNull, mdlFull)
+# 
+# summary(objLme)
+
+#library(ggplot2)
+#ggplot(data=objDf, aes(x=Depth, y=PSC, group=ROI, color=ROI)) +
+#  geom_line()
+
+

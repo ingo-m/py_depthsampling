93ba075d83e49084285ee6dc6314b059003b06bd
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-01-22 21:06:34 +0100

    Model adjustments.

--- py_depthsampling/lme/r_depth_analysis.r
+++ py_depthsampling/lme/r_depth_analysis.r
@@ -18,40 +18,39 @@ head(objDf)
 mdl_lme = lme(PSC ~ ROI * Condition * Depth,
               objDf,
               random=(~1|Subject),
-              correlation=corCAR1(form=(~1|Depth)))
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              method='ML')
 
-# Linear model using generalised least squares:
-mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
+# # Linear model using generalised least squares:
+# mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
+#               objDf,
+#               correlation=corCAR1(form=(~1|Depth)),
+#               method='ML')
+# 
+# summary(mdl_gls)
+# 
+# nlme:::summary.gls(mdl_gls)$tTable
+
+mdlNull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Condition +
+              ROI:Depth +
+              Condition:Depth,
               objDf,
-              method='ML',
-              correlation=corCAR1(form=(~1|Depth)))
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              method='ML')
 
-summary(mdl_gls)
+mdlFull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Condition +
+              ROI:Depth +
+              Condition:Depth +
+              Condition:Depth:ROI,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              method='ML')
 
-nlme:::summary.gls(mdl_gls)$tTable
-
-
-
-# mdlNull = lme(PSC ~ ROI + Condition + Depth +
-#               ROI:Condition +
-#               ROI:Depth +
-#               Condition:Depth,
-#               objDf,
-#               random=(~1|Subject),
-#               method='ML')
-# 
-# mdlFull = lme(PSC ~ ROI + Condition + Depth +
-#               ROI:Condition +
-#               ROI:Depth +
-#               Condition:Depth +
-#               Condition:Depth:ROI,
-#               objDf,
-#               random=(~1|Subject),
-#               method='ML')
-# 
-# anova(mdlNull, mdlFull)
-# 
-# summary(objLme)
+anova(mdlNull, mdlFull)
 
 #library(ggplot2)
 #ggplot(data=objDf, aes(x=Depth, y=PSC, group=ROI, color=ROI)) +

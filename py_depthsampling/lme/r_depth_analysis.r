d88bf542ae8353cbf885ee08bad43cd5500a7da6
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-01-29 22:46:04 +0100

    Model comparison for effect of ROI:Condition and ROI:Condition:Depth.

--- py_depthsampling/lme/r_depth_analysis.r
+++ py_depthsampling/lme/r_depth_analysis.r
@@ -6,15 +6,36 @@ objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/
 
 head(objDf)
 
-# # Linear model using generalised least squares:
-# mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
-#               objDf,
-#               correlation=corCAR1(form=(~1|Depth)),
-#               method='ML')
-# 
-# summary(mdl_gls)
-# 
-# nlme:::summary.gls(mdl_gls)$tTable
+# (1)
+# Do the stimuli differentially activate the ROIs (i.e. does activation differ
+# between ROIs as a function of condition)?
+
+# Null model:
+mdlNull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Depth +
+              Condition:Depth,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              method='ML')
+
+# Full model:
+mdlFull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Condition +  # Effect of interest
+              ROI:Depth +
+              Condition:Depth,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              method='ML')
+
+# Model comparison:
+anova(mdlNull, mdlFull)
+
+# (2)
+# Is there an effect of condition on the depth profiles with respect to
+# condition (i.e. do the condition differences over cortical depth differ
+#between ROI)?
 
 # Null model:
 mdlNull = lme(PSC ~ ROI + Condition + Depth +
@@ -40,8 +61,16 @@ mdlFull = lme(PSC ~ ROI + Condition + Depth +
 # Model comparison:
 anova(mdlNull, mdlFull)
 
+# # Linear model using generalised least squares:
+# mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
+#               objDf,
+#               correlation=corCAR1(form=(~1|Depth)),
+#               method='ML')
+#
+# summary(mdl_gls)
+#
+# nlme:::summary.gls(mdl_gls)$tTable
+
 #library(ggplot2)
 #ggplot(data=objDf, aes(x=Depth, y=PSC, group=ROI, color=ROI)) +
 #  geom_line()
-
-

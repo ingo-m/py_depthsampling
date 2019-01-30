852f5c8e3329264c7a63748aec943920c80b450b
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-01-30 21:58:42 +0100

    Explicit type (floating point).

--- py_depthsampling/permutation/peak_pos_perm_diff.py
+++ py_depthsampling/permutation/peak_pos_perm_diff.py
@@ -251,7 +251,8 @@ def peak_diff(strPthData, lstDiff, lstCon, varNumIt=1000, varThr=0.05):
     vecPermPeaksB, vecLgcB = find_peak(aryDpthRndB, varThr=varThr)
 
     # Ratio of iterations with peak:
-    varRatioPeak = (np.sum(vecLgcA) + np.sum(vecLgcB)) / (2.0 * varNumIt)
+    varRatioPeak = (float(np.sum(vecLgcA) + np.sum(vecLgcB))
+                    / float(2.0 * varNumIt))
     print(('------Percentage of permutation samples with peak: '
           + str(np.around(varRatioPeak, decimals=3))))
 

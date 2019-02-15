70985a436e5e6ee50a702bb05917f4a7a41e9e43
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-02-15 17:37:13 +0100

    Consider number of vertices (i.e. size of ROI).

--- py_depthsampling/lme/r_depth_analysis.r
+++ py_depthsampling/lme/r_depth_analysis.r
@@ -2,11 +2,13 @@
 library(nlme)
 
 # Read CSV into R
-objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/depth_data.csv', header=TRUE, sep=';')
+objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/surface_depth_data_centre.csv', header=TRUE, sep=';')
 
 head(objDf)
 
-# (1)
+# Weights = ~1/n where n is number of vertices.
+
+# (1) - Two-way interaction
 # Do the stimuli differentially activate the ROIs (i.e. does activation differ
 # between ROIs as a function of condition)?
 
@@ -17,6 +19,7 @@ mdlNull = lme(PSC ~ ROI + Condition + Depth +
               objDf,
               random=(~1|Subject),
               correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
               method='ML')
 
 # Full model:
@@ -27,15 +30,42 @@ mdlFull = lme(PSC ~ ROI + Condition + Depth +
               objDf,
               random=(~1|Subject),
               correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
               method='ML')
 
 # Model comparison:
 anova(mdlNull, mdlFull)
 
-# (2)
-# Is there an effect of condition on the depth profiles with respect to
-# condition (i.e. do the condition differences over cortical depth differ
-#between ROI)?
+# (2) - Two-way interaction
+# Do the stimuli differentially activate the cortical depth levels (i.e. does
+# activation differ between depth levels as a function of condition)?
+
+# Null model:
+mdlNull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Condition +
+              ROI:Depth,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
+              method='ML')
+
+# Full model:
+mdlFull = lme(PSC ~ ROI + Condition + Depth +
+              ROI:Condition +
+              ROI:Depth +
+              Condition:Depth,  # Effect of interest
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
+              method='ML')
+
+# Model comparison:
+anova(mdlNull, mdlFull)
+
+# (3) Three-way interaction
+# Do the condition differences over cortical depth differ between ROIs?
 
 # Null model:
 mdlNull = lme(PSC ~ ROI + Condition + Depth +
@@ -45,6 +75,7 @@ mdlNull = lme(PSC ~ ROI + Condition + Depth +
               objDf,
               random=(~1|Subject),
               correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
               method='ML')
 
 # Full model:
@@ -52,24 +83,41 @@ mdlFull = lme(PSC ~ ROI + Condition + Depth +
               ROI:Condition +
               ROI:Depth +
               Condition:Depth +
-              Condition:Depth:ROI,  # Effect of interest
+              ROI:Condition:Depth,  # Effect of interest
               objDf,
               random=(~1|Subject),
               correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
               method='ML')
 
 # Model comparison:
 anova(mdlNull, mdlFull)
 
-# # Linear model using generalised least squares:
-# mdl_gls = gls(PSC ~ ROI * Condition * Depth * Subject,
-#               objDf,
-#               correlation=corCAR1(form=(~1|Depth)),
-#               method='ML')
-#
-# summary(mdl_gls)
-#
-# nlme:::summary.gls(mdl_gls)$tTable
+# (4)
+# Is there a difference in activation between conditions 'Kanizsa' and 'Kanizsa
+# rotated'?
+
+# Select 'Kanizsa' and 'Kanizsa roated' conditions:
+objDfKonly <- objDf[objDf$Condition!='BRIGHT_SQUARE',]
+
+# Null model:
+mdlNull = lme(PSC ~ ROI + Depth,
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
+              method='ML')
+
+# Full model:
+mdlFull = lme(PSC ~ ROI + Condition + Depth,  # Effect of interest = Condition
+              objDf,
+              random=(~1|Subject),
+              correlation=corCAR1(form=(~1|Subject/Depth)),
+              weights=(~1/Vertices),
+              method='ML')
+
+# Model comparison:
+anova(mdlNull, mdlFull)
 
 #library(ggplot2)
 #ggplot(data=objDf, aes(x=Depth, y=PSC, group=ROI, color=ROI)) +

1b37b16d7421710c655e617c7b97cb9aebea4f3e
Ingo Marquardt <ingo.marquardt@gmx.de>
2019-04-13 14:02:11 +0200

    Fit linear mixed model (and ANOVA).

--- py_depthsampling/lme/r_anova_deep_gm_only.r
+++ py_depthsampling/lme/r_anova_deep_gm_only.r
@@ -2,12 +2,17 @@
 # ANOVA for effect of ROI #
 ###########################
 
+# Only needed for regression model (see below).
+library(nlme)
+
+# (1)
 # Perform an ANOVA for an effect of ROI separately at deep or middle or
 # superficial cortical depth.
 
-# Read CSV into R
-objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/depth_data_stimulus.csv', header=TRUE, sep=';')
-# objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/depth_data_periphery.csv', header=TRUE, sep=';')
+# Read CSV into R:
+objDf <- read.csv(file='/home/john/PhD/GitLab/py_depthsampling/py_depthsampling/lme/depth_data_stimulus_nvertices.csv',
+                  header=TRUE,
+                  sep=';')
 
 # Test only PacMan dynamic and control dynamic conditions, exclude PacMan static
 # condition:
@@ -23,7 +28,27 @@ objDf <- objDf[objDf$Depth<3,]
 objDf <- aggregate(PSC ~ ROI + Condition + Subject, objDf, mean)
 
 # Fit ANOVA model:
-objAov <-  aov(PSC ~ ROI*Condition + Subject, data=objDf)
-# Weighting?
+objAov <-  aov(PSC ~ ROI * Condition + Subject,
+               data=objDf,
+               weights=objDf$Vertices)
 
 summary(objAov)
+
+# (2)
+# Linear mixed model comparison.
+
+# Full model:
+mdlFull = lme(PSC ~ ROI + Condition +
+              ROI:Condition,  # Effect of interest
+              objDf,
+              random=(~1|Subject),
+              method='ML')
+
+# Null model:
+mdlNull = lme(PSC ~ ROI + Condition,
+              objDf,
+              random=(~1|Subject),
+              method='ML')
+
+# Model comparison:
+anova(mdlNull, mdlFull)

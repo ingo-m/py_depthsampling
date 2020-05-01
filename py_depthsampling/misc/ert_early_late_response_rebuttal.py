e9ea05e9f065f9417cc1cdff460cf63b3e6c9798
Ingo Marquardt <ingo.marquardt@gmx.de>
2020-05-01 19:16:58 +0200

    Create npz files with depth profiles based on event-related timecourses, to be used for draining effect removal.

--- py_depthsampling/misc/ert_early_late_response_rebuttal.py
+++ py_depthsampling/misc/ert_early_late_response_rebuttal.py
@@ -29,6 +29,11 @@ from py_depthsampling.drain_model.drain_model_main import drain_model
 # Path of pickle files with event-related timecourses (ROI left open):
 pathPic = '/media/ssd_dropbox/Dropbox/University/PhD/PacMan_Depth_Data/Higher_Level_Analysis/stimulus/era_{}_rh.pickle'
 
+# Path for npz files with depth profiles (both for intermediate files to
+# perform draining correction, and corrected depth profiles; file name left
+# open):
+pathNpz = '/media/ssd_dropbox/Dropbox/University/PhD/PacMan_Depth_Data/Higher_Level_Analysis/stimulus/{}_rh.npz'
+
 # List of ROIs:
 lstRois = ['v1', 'v2', 'v3']
 
@@ -42,8 +47,12 @@ lstTmeWins = [(5, 6, 7),
 
 
 # -----------------------------------------------------------------------------
-# *** Loop through ROIs
+# *** Create npz files with depth profiles
 
+# Create npz files with depth profiles based on event-related timecourses, to
+# be used for draining effect removal.
+
+# Loop through ROIs
 for strRoi in lstRois:
 
     # Load pickle with event-related timecourses of current ROI:
@@ -59,7 +68,7 @@ for strRoi in lstRois:
     varNumDpth = list(dicAllSubsRoiErt.values())[0][0].shape[1]
 
     # Loop through time windows and construct arrays for drain model.
-    for tplTmeWin in lstTmeWins:
+    for idxTmeWin, tplTmeWin in enumerate(lstTmeWins):
 
         # To apply the drain model, we need to save depth profiles to numpy
         # arrays with shape aryDpth[subject, condition, depth].
@@ -69,11 +78,27 @@ for strRoi in lstRois:
         vecNumInc = np.zeros(varNumSubs)
 
         # Loop through subjects:
-        for count, strSub in enumerate(list(dicAllSubsRoiErt.keys())):
+        for idxSub, strSub in enumerate(list(dicAllSubsRoiErt.keys())):
 
             # Take mean over time for volumes in current time window, for
             # current subject:
-            aryDpth[count, :, :] = np.mean(dicAllSubsRoiErt[strSub][0]
-                                           [:, :, tplTmeWin],
-                                           axis=2)
+            aryDpth[idxSub, :, :] = np.mean(dicAllSubsRoiErt[strSub][0]
+                                            [:, :, tplTmeWin],
+                                            axis=2)
+
+            # Number of vertices for current subject:
+            vecNumInc[idxSub] = dicAllSubsRoiErt[strSub][1]
+
+        # Path for npz file:
+        pathNpzTmp = pathNpz.format((strRoi
+                                     + '_dpth_prfls_time_win_'
+                                     + str(idxTmeWin)))
+
+        # Save npz file with depth profiles of current time window to disk:
+        np.savez(pathNpzTmp,
+                 aryDpth=aryDpth,
+                 vecNumInc=vecNumInc)
+
+
+
 

b748cc549a300a7e680f9a0ced07d5b6e04007fd
Ingo Marquardt <ingo.marquardt@gmx.de>
2020-05-01 18:24:49 +0200

    Construct arrays with single subject depth profiles.

--- py_depthsampling/misc/ert_early_late_response_rebuttal.py
+++ py_depthsampling/misc/ert_early_late_response_rebuttal.py
@@ -19,6 +19,8 @@ Reviewer comment:
 
 import pickle
 import numpy as np
+from py_depthsampling.diff.diff_sem import diff_sem
+from py_depthsampling.drain_model.drain_model_main import drain_model
 
 
 # -----------------------------------------------------------------------------
@@ -30,7 +32,13 @@ pathPic = '/media/ssd_dropbox/Dropbox/University/PhD/PacMan_Depth_Data/Higher_Le
 # List of ROIs:
 lstRois = ['v1', 'v2', 'v3']
 
-# ...
+# For the PacMan project, the event related pickled timecourses should contain
+# 19 time points. The index of the first volume during which the stimulus was
+# on is 5, and the last volume during which the stimulus was on is 10. Define
+# for which time windows to construct new depth profiles (list of tuples with
+# volume indices):
+lstTmeWins = [(5, 6, 7),
+              (8, 9, 10)]
 
 
 # -----------------------------------------------------------------------------
@@ -41,4 +49,31 @@ for strRoi in lstRois:
     # Load pickle with event-related timecourses of current ROI:
     dicAllSubsRoiErt = pickle.load(open(pathPic.format(strRoi), 'rb'))
 
+    # Number of subjects:
+    varNumSubs = len(dicAllSubsRoiErt)
+
+    # Number of conditions:
+    varNumCon = list(dicAllSubsRoiErt.values())[0][0].shape[0]
+
+    # Number of depth levels:
+    varNumDpth = list(dicAllSubsRoiErt.values())[0][0].shape[1]
+
+    # Loop through time windows and construct arrays for drain model.
+    for tplTmeWin in lstTmeWins:
+
+        # To apply the drain model, we need to save depth profiles to numpy
+        # arrays with shape aryDpth[subject, condition, depth].
+        aryDpth = np.zeros((varNumSubs, varNumCon, varNumDpth))
+
+        # Array for number of vertices per subject:
+        vecNumInc = np.zeros(varNumSubs)
+
+        # Loop through subjects:
+        for count, strSub in enumerate(list(dicAllSubsRoiErt.keys())):
+
+            # Take mean over time for volumes in current time window, for
+            # current subject:
+            aryDpth[count, :, :] = np.mean(dicAllSubsRoiErt[strSub][0]
+                                           [:, :, tplTmeWin],
+                                           axis=2)
 
